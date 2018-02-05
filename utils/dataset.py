1a610d5337239b2d07fc8bf1c9ffff3fc8c65bf2
estelle1722 <1922525328@qq.com>
2018-02-05 15:02:04 +0800

    scan dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,23 +1,24 @@
 from torchvision import datasets,transforms
 from torch.utils.data import DataLoader
+import torch
 
 train_mnist = datasets.MNIST('../dataset/mnist', train=True, download=True, transform=transforms.ToTensor())
 test_mnist = datasets.MNIST('../dataset/mnist', train = False, download=True, transform=transforms.ToTensor())
 
-train_loader = DataLoader(train_mnist, batch_size=1, shuffle=True)
-test_loader = DataLoader(test_mnist, batch_size=1, shuffle=False)
+train_loader = DataLoader(train_mnist, batch_size=10, shuffle=True)
+test_loader = DataLoader(test_mnist, batch_size=10, shuffle=False)
 
-assert len(train_loader) == 60000
-assert len(test_loader) == 10000
 assert len(test_mnist) == 10000
 assert len(train_mnist) == 60000
-
-
-# assert len(train_loader) == 50000
+# size of image:(channels, height, width)
+assert (1, 28, 28) == (train_mnist[0][0]).size()
+# the type of image
+assert type(train_mnist[0][0]) == torch.FloatTensor
 
 for i,data in enumerate(train_loader, 1):
+    # <class 'torch.LongTensor'>
     img,label = data
     img = img.view(img.size(0), -1)
     assert img.size(1) == 784
-    if i == 5:
+    if i == 1:
         break
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
-
+import torch
+from torchvision import datasets,transforms
+import numpy as np
 
 def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=5):
     """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
@@ -8,3 +10,24 @@ def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=5):
         param_group['lr'] = lr
 
     return optimizer
+
+def set_parameters_static(model):
+    for para in list(model.parameters()):
+        para.requires_grad = False
+    return model
+
+
+def get_img_means():
+    train_mnist = datasets.MNIST('../dataset/mnist', train=True, download=True, transform=transforms.ToTensor())
+
+    mnist_imgs = [data[0].view(1, 784).numpy() for data in train_mnist]
+    mnist_labels = [data[1] for data in train_mnist]
+
+    imgs_means = np.zeros((10, 784))
+    for i in range(10):
+        imgs = []
+        for index, label in enumerate(mnist_labels):
+            if label == i:
+                imgs.append(mnist_imgs[index])
+        imgs_means[i, :] = np.mean(imgs, axis=0)
+    return torch.from_numpy(imgs_means).type(torch.FloatTensor)
