6421b1018d007b8326b23e3c148b6a0832888555
estelle1722 <1922525328@qq.com>
2018-02-10 14:18:50 +0800

    debug utils

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,8 +5,9 @@ import torch
 train_mnist = datasets.MNIST('../dataset/mnist', train=True, download=True, transform=transforms.ToTensor())
 test_mnist = datasets.MNIST('../dataset/mnist', train = False, download=True, transform=transforms.ToTensor())
 
-train_loader = DataLoader(train_mnist, batch_size=10, shuffle=True)
-test_loader = DataLoader(test_mnist, batch_size=10, shuffle=False)
+batch_size = 32
+train_loader = DataLoader(train_mnist, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_mnist, batch_size=batch_size, shuffle=False)
 
 assert len(test_mnist) == 10000
 assert len(train_mnist) == 60000
@@ -18,6 +19,7 @@ assert type(train_mnist[0][0]) == torch.FloatTensor
 for i,data in enumerate(train_loader, 1):
     # <class 'torch.LongTensor'>
     img,label = data
+    assert img.size() == (batch_size, 1, 28, 28)
     img = img.view(img.size(0), -1)
     assert img.size(1) == 784
     if i == 1:
